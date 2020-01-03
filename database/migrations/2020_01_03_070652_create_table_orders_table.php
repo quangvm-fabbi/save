003c5b44da0f2ffde8fd344638075d6edbffb4ab
Quang <quangvm1998@gmail.com>
2020-01-03 16:07:23 +0700

    Add migration

---
+++ database/migrations/2020_01_03_070652_create_table_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('total_quanity');
+            $table->double('total_price');
+            $table->string('note');
+            $table->tinyInteger('status');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
