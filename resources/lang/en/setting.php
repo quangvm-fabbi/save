40d715276d5b42d138cf5b56e4c088258513266a
Quang <quangvm1998@gmail.com>
2020-01-04 00:41:05 +0700

    Add layouts admin

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'dash' => 'Dashboard',
+    'user' => 'List Users',
+    'cat' => 'List Categories',
+    'product' => 'List Cakes',
+    'order' => 'List Orders',
+    'comment' => 'List Comment',
+    'not' => 'Notifications',
+    'sea' => 'Search',
+    'acc' => 'Account',
+    'out' => 'Log out',
+    'hom' => 'Home',
+    'com' => 'Company',
+    'blog' => 'Blog',
+    'create' => 'Rose bakery',
+    'text1' => ', made with love for a better web',
+    'title1' => 'Cake shop management page',
+];
